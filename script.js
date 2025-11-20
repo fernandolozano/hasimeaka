91aff02bfe1ef0797bded842c6f71103e7134710
Fernando Lozano <fernandolozanopajaron@gmail.com>
2025-11-20 16:14:04 +0100

    New web

--- script.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const yearElement = document.getElementById('year');
-    const currentYear = new Date().getFullYear();
-    yearElement.textContent = currentYear;
-});
